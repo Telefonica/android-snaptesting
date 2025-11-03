0547d0c712d83fd9888c0b9df193ed902919ce09
Jesús Latorre <jesus.latorreasensio@telefonica.com>
2025-11-03 15:22:10 +0100

    ANDROID-17070 Add optional reason to IgnoreLogs and IgnoreScreenshots

--- android-snaptesting/src/main/java/com/telefonica/androidsnaptesting/Ignores.kt
+++ android-snaptesting/src/main/java/com/telefonica/androidsnaptesting/Ignores.kt
@@ -3,8 +3,12 @@ package com.telefonica.androidsnaptesting
 
 @Retention(AnnotationRetention.RUNTIME)
 @Target(AnnotationTarget.FUNCTION)
-public annotation class IgnoreLogs
+public annotation class IgnoreLogs(
+    val reason: String = ""
+)
 
 @Retention(AnnotationRetention.RUNTIME)
 @Target(AnnotationTarget.FUNCTION)
-public annotation class IgnoreScreenshots
+public annotation class IgnoreScreenshots(
+    val reason: String = ""
+)
